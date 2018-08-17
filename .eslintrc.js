674b30b22923fa12a88064673eadca902ffbfbb6
John <capybarainspace@gmail.com>
2018-08-17 14:13:04 +0300

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,9 +9,21 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    quotes: ['warn', 'single'],
+    indent: ['warn', 2],
+    semi: ['error', 'always'],
+    ignoreComments: true,
+    ignoreUrls: true,
+    ignoreStrings: true,
+    ignoreTemplateLiterals: true
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+        'jsx': true
+    }
   }
 }
